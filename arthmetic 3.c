d3a3d67731f408a0cbc1663935d0a758de06b199
anshusahu96 <anshusahu969568@gmail.com>
2025-04-24 18:49:28 +0530

    Add files via upload

---
+++ arthmetic 3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+	//Arthmatic opreators
+	// + - * / %
+	//Arthmatic opreator Addition
+	int a = 20;
+	int b = 90;
+	int c = a+b;
+	printf("c= %d\n",c);
+	//Arthmatic opreator Substraction
+	int d = a-b;
+	printf("d=%d\n",d);
+	// Arthmatic opreator multiplication
+	int e = a*b;
+	printf("e=%d\n",e);
+	//arthmatic opreator divison
+	float num1 = 12;
+	float num2 = 6;
+	float num3 = num1/num2;
+	printf("num3 =%f\n",num3);
+	// Arthmatic opreator for post increment
+	int num8 = 45;
+	printf("num8 = %d\n",num8++);
+	printf("num8 = %d\n",num8);
+	//Arthmetic opreator for pre increament
+	int num9 = 51;
+	printf("num9 = %d\n", ++num9);
+	
+}
