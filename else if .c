03336ba0b5dedda4e6baf568fb1ed4baece26b80
anshusahu96 <anshusahu969568@gmail.com>
2025-05-07 14:15:43 +0100

    Add files via upload

---
+++ else if .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main(){
+        int a = 20;
+        int b = 30;
+        int c = a+b;
+        printf("%d\n", c);
+        if( a >20){
+            printf("this is right number");
+        }else if(a == b){
+                printf("this is right number");
+        }else{
+            printf("Retry please");
+        }
+    }
