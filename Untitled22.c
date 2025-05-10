1ad3ae9f8bb3da26fe401ba103bfeac575182ef1
anshusahu96 <anshusahu969568@gmail.com>
2025-05-10 00:08:13 -1200

    Add files via upload

---
+++ Untitled22.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+	int main(){
+		int i;
+		// for loop
+		for( i = 1; i <=50; i++){
+			printf(" %d\t", i);
+		}
+	int b = 1;
+	// while loop
+	while(b <= 50){
+			printf("%d\t", b);
+		b++;		
+	}
+	int c = 0;
+	// do-while loop
+	do{
+		printf("%d\t", c);
+		c++;
+	}
+		while( c <= 50);
+	}
