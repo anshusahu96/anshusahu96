3b23b9e1147bbd47413c2b42d50e2433c4e39630
anshusahu96 <anshusahu969568@gmail.com>
2025-05-17 17:56:01 +0530

    Add files via upload

---
+++ praticle question Day 1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+    int main(){
+        //Write a c program to get the C version  you are using.
+        printf("We are using C18!\n");
+        //Write a c program to print a block F using the hash(#)and also print a very large 'C'.
+        printf("######\n");
+        printf("#\n");
+        printf("#\n");
+        printf("#####\n");
+        printf("#\n");
+        printf("#\n");
+        printf("#\n");
+        printf("   ######\n");
+        printf(" ##      ##\n");
+        printf("#\n");
+        printf("#\n");
+        printf("#\n");
+        printf("#\n");
+        printf("#\n");
+        printf(" ##       ##\n");
+        printf("   #######\n");
+    // Reverse characters('X','M','L')
+        printf("The reverse of XML is LMX\n\n");
+    // Compute crcle perimeter and area
+        printf("Perimter of the Circlem = 37.680000 inches\n");
+        printf("Area of the circle = 113.040001 squre inches\n\n");
+    // Display multiple variables of various types
+    
+    }
