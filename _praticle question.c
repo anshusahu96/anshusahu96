141a49a0f8aa2ce19a406fec71cb989846986790
anshusahu96 <anshusahu969568@gmail.com>
2025-05-19 19:05:27 +0530

    Add files via upload

---
+++ _praticle question.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+    int main(){
+        // Compute rectangle perimeter
+        int height , width;
+        height = 7;
+        width = 5;
+        int perimeter;
+        perimeter= height*width;
+        printf(" perimeter of the rectangle= %d icnhes\n", perimeter);
+        int area;
+        area = 2*(height + width);
+        printf("Area of the rectangle= %d squre inches\n\n", area);
+        // compute ciecle perimeter and area 
+        int redius, pi;
+        redius = scanf("%f", &redius);
+        pi = 3.14;
+        int perimeter, area;
+        perimeer = 2*pi*redius;
+        area = pi*redius*redius;
+        printf("Perimeter of the Circle= %f inches\n");
+        printf("Area of the Circle= %f squre inches\n");
+        
+         
+        
+        
+    }
